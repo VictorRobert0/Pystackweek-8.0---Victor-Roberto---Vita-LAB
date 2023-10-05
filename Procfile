730b8f17b1e9b1ac4296567de65e75e890873fa9
Victor Roberto <contatokataltd@gmail.com>
2023-10-05 18:29:44 -0300

    Projeto no railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vitalab.wsgi --log-file -
--- vitalab/settings.py
+++ vitalab/settings.py
@@ -78,11 +78,12 @@ WSGI_APPLICATION = 'vitalab.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+   'default': {
+      'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
